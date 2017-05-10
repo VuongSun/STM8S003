30881574ee382a33eaf93e80d90905937fc7d835
Vuong <minhvuong2302@gmail.com>
2017-05-10 23:15:09 +0700

    nhan ki tu gui lai luon ki tu do

--- UART/Main/main.c
+++ UART/Main/main.c
@@ -1,91 +1,62 @@
-/*****************************************************************************
-  *Ten Tep          :     main.c
-  *Ngay             :     24/12/2016
-  *Tac Gia          :     MCA team
-  *Cong Ty          :     MCA
-  *Webside          :     mca.vn
-  *Phien Ban        :     V1.0
-  *Tom Tat          :     Lap trinh su dung GPIO trong STM8S105C4T6
-  ******************************************************************************/
+/* Includes ------------------------------------------------------------------*/
+#include "stm8s.h"
+#include "stdio.h"
+#include "stm8s_uart1.h"
+#include "stm8s_conf.h"
 
-#include "main.h"
-#include "var.h"
-
-#if (__MAIN_H!=24122016)
-    #error "Include Sai File main.h"
-#endif
-
-
-/*____________________________NGAT NHAN UART__________________________________*/
-#pragma vector = UART1_R_RXNE_vector
-__interrupt void Ngat_Nhan_UART1 (void)
-{ 
-    // BAI TAP 2: Su dung dong khung de nhan du lieu
-    temp = UART1_ReceiveData8();
-    uart1_buff[i] = temp;
-    i++;
-    if(uart1_buff[0] == '%')
-    {
-        if(temp == '@') 
-        {
-            i=0;
-            n = strlen(uart1_buff);
-            memset(data, '\0',30);          // Restore buffer ve NULL
-            memcpy(data, uart1_buff+1, n-2);  // Copy du lieu vao bien data (bo dau % di)
-            //memset(uart2_buff, '\0', 10);   // Restore bo dem ve NULL
-        }   
-    }
-    else 
-    {
-        i = 0;
-        memset(uart1_buff, '\0', 10); 
-    }
-}
-
+int putchar (int c);  // functions Putchar --> UART
+int getchar (void);   // functions Getchar --> UART
+char echo;
 void main(void)
 {
-/********************************* SYSTEM *************************************/
-    CLK_HSICmd(ENABLE);                        // Cho phep su dung nguon Clock HSI
-    CLK_SYSCLKConfig(CLK_PRESCALER_HSIDIV1);   // Su dung nguon Clock co F = 16MHz  					
-/*________________________________ GPIO ___________________________________*/
-    GPIO_Configuration();
-
-/********************************* TIMER *************************************/
-/*________________________________ TIMER1 ___________________________________*/ 
+  
+  /*High speed internal clock prescaler: 1*/
+    CLK_DeInit();
+    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      //f_Master = HSI/1 = 16MHz
+    CLK_SYSCLKConfig(CLK_PRESCALER_CPUDIV1);            //f_CPU = f_Master/1 = 16MHz
+    while(CLK_GetFlagStatus(CLK_FLAG_HSIRDY)!=SET);     //wait until HSI ready
     
-/*________________________________ TIMER2 ___________________________________*/
-    TIM2_Configuration();
-/*________________________________ TIMER3 ___________________________________*/
+  
+    UART1_DeInit();
+  /* UART1 configuration ------------------------------------------------------*/
+  /* UART1 configured as follow:
+        - BaudRate = 9600 baud  
+        - Word Length = 8 Bits
+        - One Stop Bit
+        - No parity
+        - Receive and transmit enabled
+        - UART1 Clock disabled
+  */
+  UART1_Init(9600, UART1_WORDLENGTH_8D, UART1_STOPBITS_1, UART1_PARITY_NO,
+              UART1_SYNCMODE_CLOCK_DISABLE, UART1_MODE_TXRX_ENABLE);
 
-/*________________________________ TIMER4 ___________________________________*/
-    //TIM4_Configuration();	
-/********************************* UART2 **************************************/ 
-    uart1_init();    
-/********************************* IWDG ***************************************/   
-    //IWDG_Configuration();
- 
-/********************************* EEPROM *************************************/   
-    //EEPROM_Configuration();
-/*________________________________ USER ___________________________________*/
-    enableInterrupts();
-  // lcd1602_init();
-  // lcd1602_gotoxy(0,0);
-  // lcd1602_puts("Data: ");
-    while(1)
-    {
-        //BAI TAP 2: Su dung dong khung de nhan du lieu
-        memset(uart1_buff, '\0', 10);   // Restore bo dem ve NULL
-        if(strcmp(data,"ON LED")==0)
-        {
-            PORTB_5 = 0;
-            delay_ms(1000);
-        }
-        else PORTB_5 = 1;
-        uart1_puts(data);
-        uart1_putchar('\n');
-        delay_ms(500);  //*/
-    }
+  
+  
+  while (1)
+  {
+    echo = getchar();
+    printf("%c", echo);  
+   
+  }
 }
 
-/*------------------------------KET THUC FILE-------------------------------
- _________________________________MCA Team__________________________________*/ 
+/////////////////////// Functions /////////////////////////////////
+int putchar (int c)
+{
+  /* Write a character to the UART1 */
+  UART1_SendData8(c);
+  /* Loop until the end of transmission */
+  while (UART1_GetFlagStatus(UART1_FLAG_TXE) == RESET);
+
+  return (c); 
+}
+///////////////////
+
+int getchar (void)
+{
+  int c = 0;
+  /* Loop until the Read data register flag is SET */
+  while (UART1_GetFlagStatus(UART1_FLAG_RXNE) == RESET);
+    c = UART1_ReceiveData8();
+  return (c);
+}
